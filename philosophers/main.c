c7520ecb583858239b7ed8c86e7bd54ae463fdaf
Ivan Lorenzo Cutrona <ilorenzo@car11s5.42barcelona.com>
2024-02-16 13:46:35 +0100

    akka

--- philosophers/main.c
+++ philosophers/main.c
@@ -1,10 +1,47 @@
 #include "./philo.h"
 
+typedef struct s_philo
+{
+    t_params            params;
+    int                 num;
+    struct s_philo     *left;
+    struct s_philo     *right;
+}   t_philo;
+
+t_philo    *add_philo(t_philo **table, t_params params, int num)
+{
+    t_philo *new_philo;
+    t_philo *last_philo;
+
+    new_philo = malloc(sizeof(t_philo));
+    new_philo->params = params;
+    new_philo->num = num;
+
+    if (!(*table))
+    {
+        new_philo->left = NULL;
+        new_philo->right = NULL;
+        *table = new_philo;
+        return (new_philo);
+    }
+    if (!((*table)->left))
+    {
+        (*table)->left = new_philo;
+        last_philo = *table;
+    }
+    else
+        last_philo = (*table)->left;
+    last_philo->right = new_philo;
+    new_philo->left = last_philo;
+    new_philo->right = *table;
+    return (new_philo);
+}
+
 t_params    params_init(char **argv)
 {
     t_params params;
     params.number_of_philosophers = ft_atoi(argv[1]);
-    params.time_to_die = ft_atoi(argv[2]);
+    /*params.time_to_die = ft_atoi(argv[2]);
     params.time_to_eat = ft_atoi(argv[3]);
     params.time_to_sleep = ft_atoi(argv[4]);
     if (argv[5])
@@ -16,32 +53,62 @@ t_params    params_init(char **argv)
     printf("- time_to_eat: %d\n", params.time_to_eat);
     printf("- time_to_sleep: %d\n", params.time_to_sleep);
     printf("- number_of_times_each_philosopher_must_eat: %d\n", params.number_of_times_each_philosopher_must_eat);
-    printf("\n\n\n");
+    printf("\n\n\n");*/
     params.th = malloc((params.number_of_philosophers) * sizeof(pthread_t));
     return (params);
 }
 
-void    *philosopher(void *params)
+void    free_table(t_philo **table)
 {
-    t_params    *param;
+    t_philo *next;
+    t_philo *current;
 
-    param = (t_params *)params;
-    printf("Hola\n");
-    return ((void *) param);
+    current = *table;
+    next = current->right;
+    free(current);
+    current = next;
+    while (current != *table)
+    {
+        next = current->right;
+        printf("free: %d\n", current->num);
+        free(current);
+        current = next;
+    }
+    *table = NULL;
+}
+void    *philosopher(void *philo)
+{
+    t_philo    *my_philo;
+
+    my_philo = (t_philo *)philo;
+    printf("Hola, aqui filosofo %d, derecha: %d, izquierda: %d\n", my_philo->num, my_philo->right->num, my_philo->left->num);
+    return ((void *) philo);
 }
 
 int	main(int argc, char **argv)
 {
     t_params    params;
     int         i;
+    t_philo     *table;
+    t_philo     *new_philo;
 
+    table = NULL;
     i = 0;
-	if (argc < 5 || argc > 6)
+	if (argc > 2)
 		return (printf("invalid arg\n"));
     params = params_init(argv);
+    while(i < params.number_of_philosophers)
+    {
+        add_philo(&table, params, i);
+        i++;
+    }
+    i = 0;
+    new_philo = table;
     while (i < params.number_of_philosophers)
     {
-        pthread_create(&(params.th[i]), NULL, philosopher, &params);
+        pthread_create(&(params.th[i]), NULL, philosopher, new_philo);
+        new_philo = new_philo->right;
+        usleep(100);
         i++;
     }
     i = 0;
@@ -52,5 +119,6 @@ int	main(int argc, char **argv)
         i++;
     }
     free(params.th);
+    free_table(&table);
     return (0);
 }
