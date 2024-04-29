8489aa3a34b81e8bbeac625cad8f86c143b4acfa
Ivan Lorenzo Cutrona <ilorenzo@car8s6.42barcelona.com>
2024-04-29 11:21:13 +0200

    reparieren

---
+++ philosophers/philo/srcs/get_set.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_set.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilorenzo <ilorenzo@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:14:38 by ilorenzo          #+#    #+#             */
+/*   Updated: 2024/04/29 11:14:40 by ilorenzo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/philo.h"
+
+int get_val(int *val, pthread_mutex_t *lock)
+{
+    int res;
+
+    pthread_mutex_lock(lock);
+    res = *val;
+    pthread_mutex_unlock(lock);
+    return (res);
+}
+
+void set_val(int *val, pthread_mutex_t *lock, int new_val)
+{
+    pthread_mutex_lock(lock);
+    *val = new_val;
+    pthread_mutex_unlock(lock);
+    return ;
+}
